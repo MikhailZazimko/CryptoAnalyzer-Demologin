75095e849594a553857c04c238341e825dc13f38
Alexander Khmelov <a.khmelev@gmail.com>
2022-03-10 21:50:57 +0300

    Add path to TXT

--- src/main/java/ru/javarush/khmelov/cryptoanalizer/constants/Constants.java
+++ src/main/java/ru/javarush/khmelov/cryptoanalizer/constants/Constants.java
@@ -1,5 +1,7 @@
 package ru.javarush.khmelov.cryptoanalizer.constants;
 
+import java.io.File;
+
 public class Constants {
     private static final String rus = "ЙЦУКЕНГШЩЗХЪЭЖДЛОРПАВЫФЯЧСМИТЬБЮ";
     private static final String eng = "QWERTYUIOPLKJHGFDSAZXCVBNM";
@@ -7,5 +9,8 @@ public class Constants {
     private static final String z = "!@#$%^&*()[]{}";
 
     public static final String ALPHABET = rus + eng + rus.toLowerCase() + eng.toLowerCase() + cypher + z;
-
+    public static final String TXT_FOLDER=System.getProperty("user.dir")+
+            File.separator+
+            "text"+
+            File.separator;
 }
