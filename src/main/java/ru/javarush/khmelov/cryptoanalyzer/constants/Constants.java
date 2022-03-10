ed97058fa8e5d7977419d8653402f1682ad852ff
Alexander Khmelov <a.khmelev@gmail.com>
2022-03-10 22:44:04 +0300

    Fix name (syntax)

--- src/main/java/ru/javarush/khmelov/cryptoanalyzer/constants/Constants.java
+++ src/main/java/ru/javarush/khmelov/cryptoanalyzer/constants/Constants.java
@@ -6,9 +6,9 @@ public class Constants {
     private static final String rus = "ЙЦУКЕНГШЩЗХЪЭЖДЛОРПАВЫФЯЧСМИТЬБЮ";
     private static final String eng = "QWERTYUIOPLKJHGFDSAZXCVBNM";
     private static final String cypher = "0123456789";
-    private static final String z = "!@#$%^&*()[]{}";
+    private static final String symbols = "!@#$%^&*()[]{}";
 
-    public static final String ALPHABET = rus + eng + rus.toLowerCase() + eng.toLowerCase() + cypher + z;
+    public static final String ALPHABET = rus + eng + rus.toLowerCase() + eng.toLowerCase() + cypher + symbols;
     public static final String TXT_FOLDER=System.getProperty("user.dir")+
             File.separator+
             "text"+
