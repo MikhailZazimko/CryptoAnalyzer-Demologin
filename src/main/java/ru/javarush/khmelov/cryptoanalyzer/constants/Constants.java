3f71de0791a9aaf3a3fc01ea9d3e7985a2cd47c7
Alexander Khmelov <a.khmelev@gmail.com>
2022-03-11 17:29:40 +0300

    Add symbols

--- src/main/java/ru/javarush/khmelov/cryptoanalyzer/constants/Constants.java
+++ src/main/java/ru/javarush/khmelov/cryptoanalyzer/constants/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     private static final String rus = "ЙЦУКЕНГШЩЗХЪЭЖДЛОРПАВЫФЯЧСМИТЬБЮ";
     private static final String eng = "QWERTYUIOPLKJHGFDSAZXCVBNM";
     private static final String cypher = "0123456789";
-    private static final String symbols = "!@#$%^&*()[]{}";
+    private static final String symbols = "!@#$%^&*()[]{}?.,;:'\"";
 
     public static final String ALPHABET = rus + eng + rus.toLowerCase() + eng.toLowerCase() + cypher + symbols;
     public static final String TXT_FOLDER=System.getProperty("user.dir")+
