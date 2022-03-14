b055c75aaff59268e79b61a4af28577622f6909a
Alexander Khmelov <a.khmelev@gmail.com>
2022-03-14 21:34:30 +0300

    fix syntax

--- src/main/java/ru/javarush/khmelov/cryptoanalyzer/commands/BruteForce.java
+++ src/main/java/ru/javarush/khmelov/cryptoanalyzer/commands/BruteForce.java
@@ -3,7 +3,7 @@ package ru.javarush.khmelov.cryptoanalyzer.commands;
 import ru.javarush.khmelov.cryptoanalyzer.entity.Result;
 import ru.javarush.khmelov.cryptoanalyzer.entity.ResultCode;
 
-public class BrudeForce implements Action{
+public class BruteForce implements Action{
     @Override
     public Result execute(String[] parameters)  {
         //TODO something do
--- src/main/java/ru/javarush/khmelov/cryptoanalyzer/controllers/Actions.java
+++ src/main/java/ru/javarush/khmelov/cryptoanalyzer/controllers/Actions.java
@@ -1,13 +1,15 @@
 package ru.javarush.khmelov.cryptoanalyzer.controllers;
 
 import ru.javarush.khmelov.cryptoanalyzer.commands.Action;
+import ru.javarush.khmelov.cryptoanalyzer.commands.BruteForce;
 import ru.javarush.khmelov.cryptoanalyzer.commands.Decoder;
 import ru.javarush.khmelov.cryptoanalyzer.commands.Encoder;
 import ru.javarush.khmelov.cryptoanalyzer.exceptions.AppException;
 
 public enum Actions {
     ENCODE(new Encoder()),
-    DECODE(new Decoder());
+    DECODE(new Decoder()),
+    BRUTEFORCE(new BruteForce());
 
     private final Action action;
 
