29b8365e1e5be4f6ba48456efdbd388270db8c19
Alexander Khmelov <a.khmelev@gmail.com>
2022-03-14 18:41:40 +0300

    next lesson

--- src/main/java/ru/javarush/khmelov/cryptoanalyzer/ConsoleRunner.java
+++ src/main/java/ru/javarush/khmelov/cryptoanalyzer/ConsoleRunner.java
@@ -4,9 +4,11 @@ import ru.javarush.khmelov.cryptoanalyzer.entity.Result;
 
 public class ConsoleRunner {
     public static void main(String[] args) {
+
         Application application = new Application();
         Result result=application.run(args);
         System.out.println(result);
+
     }
 }
 
